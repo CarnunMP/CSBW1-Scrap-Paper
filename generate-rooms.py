ecb4e23f05a472802eefa0e51cfbccb2b5885733
CarnunMP <carnun@hotmail.co.uk>
2020-03-02 15:50:59 +0000

    Descriptions are getting more complex!

--- generate-rooms.py
+++ generate-rooms.py
@@ -1,21 +1,31 @@
 import tracery
 from tracery.modifiers import base_english
 
-basicRoomFeatures = 'panels,lights,exposed wires'
+basicRoomFeatures = 'panel,light,cable'
 
 rules = {
     'setRoomName': [
-        f'[roomName:room][features:{basicRoomFeatures}]',
-        f'[roomName:corridor][features:{basicRoomFeatures}]', 
-        f'[roomName:medbay][features:{basicRoomFeatures},medical supplies,empty beds]'
+        f'[roomName:room][feature:{basicRoomFeatures}]',
+        f'[roomName:corridor][feature:{basicRoomFeatures}]', 
+        f'[roomName:medbay][feature:{basicRoomFeatures},medkit,empty bed]',
+        f'[roomName:armoury][feature:{basicRoomFeatures},phaser,pistol,assault rifle,grenade,odd-looking gun]',
+        f'[roomName:quarters][feature:{basicRoomFeatures},mirror,jewel,family photo,picture of a dog]',
+        f'[roomName:bridge][feature:{basicRoomFeatures},chair,flight computer]',
+        f'[roomName:brig][feature:{basicRoomFeatures},force field,handcuff]',
+        f'[roomName:passageway][feature:{basicRoomFeatures},communicator]',
+        f'[roomName:laboratory][feature:{basicRoomFeatures},test tube,centrifuge,lazer]',
+        f'[roomName:mess-hall][feature:{basicRoomFeatures},pot,pan,vegetable,fruit,plate,knife,fork,replicator]',
+        f'[roomName:engine-room][feature:{basicRoomFeatures},pipe,reactor,green gelatinous puddle]',
     ],
-    # 'roomName': ['room', 'corridor', 'medbay', 'armoury', 'quarters', 'bridge', 'brig', 'passageway', 'laboratory', 'mess-hall', 'engine-room'],
     'adjective': ['dark', 'smoky', 'dangerous-looking', 'messy', 'quiet', 'irradiated', 'full', 'empty', 'packed', 'stripped', 'ransacked'],
     'adverb': ['suspiciously', 'strangely', 'worryingly', 'puzzlingly', 'suprisingly', 'curiously'],
     'setTitleType': ['#adjective.capitalize#', '#adverb.capitalize# #adjective.capitalize#'],
     'title': '#setTitleType# #roomName.capitalize#',
 
-    'description': 'You see some #features#.',
+    'description': [
+        'You see #feature.a#.',
+        'You bend over, and spot several #feature.s#.'
+    ],
 
     'origin': '#[#setRoomName#]title#: #description#'
 }
