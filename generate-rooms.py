d0a3f40757992fda3f1720528add2139b69c65c7
CarnunMP <carnun@hotmail.co.uk>
2020-03-02 13:32:04 +0000

    Added some more variety to room names.

--- generate-rooms.py
+++ generate-rooms.py
@@ -2,18 +2,18 @@ import tracery
 from tracery.modifiers import base_english
 
 rules = {
-    'name': ['corridor'],
-    'adjective': ['dark', 'smoky', 'dangerous-looking', 'messy'],
-    'adverb': ['suspiciously', 'strangely', 'worryingly'],
+    'roomName': ['room', 'corridor', 'medbay', 'armoury', 'quarters', 'bridge', 'brig', 'passageway', 'laboratory', 'mess-hall', 'engine-room'],
+    'adjective': ['dark', 'smoky', 'dangerous-looking', 'messy', 'quiet', 'irradiated', 'full', 'empty', 'packed', 'stripped', 'ransacked'],
+    'adverb': ['suspiciously', 'strangely', 'worryingly', 'puzzlingly', 'suprisingly', 'curiously'],
     'setTitleType': ['#adjective.capitalize#', '#adverb.capitalize# #adjective.capitalize#'],
-    'title': '#setTitleType# #name.capitalize#',
+    'title': '#setTitleType# #roomName.capitalize#',
 }
 
 grammar = tracery.Grammar(rules)
 grammar.add_modifiers(base_english)
 
 rooms = []
-for i in range(10):
+for i in range(50):
     rooms.append(grammar.flatten('#title#'))
 
 print(rooms)
